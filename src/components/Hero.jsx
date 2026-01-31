8fc7cdb12867a51825a352cb1664b250c6f69201
Zafar Muhammed Akram <akramzm207@gmail.com>
2026-01-31 22:46:19 +0600

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
         {/* Avatar */}
         <div className="mb-8 animate-fadeIn">
           <img
-            src=".\src\image\dp.png"
+            src="https://i.ibb.co.com/cSTSk993/Chat-GPT-Image-Jan-31-2026-10-27-57-PM.png"
             alt="Zafar Muhammed Akram"
             onClick={() => setImageOpen(true)}
             className="w-48 h-48 mx-auto rounded-full object-cover border-4 border-primary-600 shadow-lg hover:shadow-2xl hover:scale-105 transition-all cursor-pointer"
